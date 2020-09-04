5d38c575e94c01fbbdb53ab0eeac8847864287a4
renarsaar <renar.saaremets@gmail.com>
2020-09-04 19:49:52 +0300

    setup database and routes

--- server/server.js
+++ server/server.js
@@ -1,6 +1,19 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./db');
+
+// Load config
+dotenv.config({ path: './.env' });
+
+connectDB();
+
 const app = express();
-const mongoose = require('mongoose');
+
+app.use(express.json());
+
+// Route Middlewares
+app.use('/products', require('./routes/products'));
+app.use('/users', require('./routes/users'));
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
