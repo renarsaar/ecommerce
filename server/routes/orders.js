4606c1192fc4479472b2df478f9cab4affba0ce9
renarsaar <renar.saaremets@gmail.com>
2021-02-03 06:45:24 +0200

    routes to get all orders & orders from 1 user

--- server/routes/orders.js
+++ server/routes/orders.js
@@ -4,6 +4,26 @@ const Order = require('../model/Order');
 const Product = require('../model/Product');
 const { makeOrderValidation } = require('../validation');
 
+// @desc    Get all orders
+// @route   GET /orders/
+router.get('/', paginatedResults(Order), async (req, res) => {
+  try {
+    res.status(200).json(res.paginatedResults);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+// @desc    Get all orders made from 1 user
+// @route   GET /orders/
+router.get('/user/:userName', paginatedResults(Order), async (req, res) => {
+  try {
+    res.status(200).json(res.paginatedResults);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
 // @desc    Make a new order
 // @route   POST /orders
 router.post('/', async (req, res) => {
@@ -52,5 +72,50 @@ router.get('/omniva', async (req, res) => {
     .then((response) => res.send(response.data))
     .catch((error) => res.status(500).send({ message: error.message }));
 });
+
+function paginatedResults(model) {
+  return async (req, res, next) => {
+    const page = parseInt(req.query.page);
+    const limit = parseInt(req.query.limit);
+
+    const startIndex = (page - 1) * limit;
+    const endIndex = page * limit;
+
+    const results = {};
+
+    if (endIndex < await model.countDocuments().exec()) {
+      results.next = {
+        page: page + 1,
+        limit,
+      };
+    }
+
+    if (startIndex > 0) {
+      results.previous = {
+        page: page - 1,
+        limit,
+      };
+    }
+
+    try {
+      results.results = await model.find();
+
+      // Filter orders by username if username in params
+      if (req.params.userName) {
+        results.paginatedResults = await model.find({ user: req.params.userName })
+          .limit(limit)
+          .skip(startIndex)
+          .exec();
+      } else {
+        results.paginatedResults = await model.find().limit(limit).skip(startIndex).exec();
+      }
+
+      res.paginatedResults = results;
+      next();
+    } catch (err) {
+      res.status(500).json({ message: err.message });
+    }
+  };
+}
 
 module.exports = router;
