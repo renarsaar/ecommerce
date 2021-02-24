2de9a2d233e92f6364ac3be2f44bed70c08a4ce9
renarsaar <renar.saaremets@gmail.com>
2021-02-24 19:34:52 +0200

    rm console.log

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -57,7 +57,6 @@ router.post('/register', async (req, res) => {
     const newUser = await user.save();
     res.status(201).send(newUser);
   } catch (err) {
-    console.log(err.message)
     res.status(400).send(err.message);
   }
 });
