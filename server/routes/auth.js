22392ac616db4bda1b2b004fc352bb16e56e997f
renarsaar <renar.saaremets@gmail.com>
2021-04-02 22:23:30 +0300

    change auth/users route respone type

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -26,7 +26,7 @@ router.get('/users', auth, paginatedResults(User), async (req, res) => {
   try {
     res.status(200).json(res.paginatedResults);
   } catch (err) {
-    res.status(500).json({ message: err.message });
+    res.status(500).json(err.message);
   }
 });
 
