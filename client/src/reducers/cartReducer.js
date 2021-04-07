1f869657a2bf99f621b71190325dd8fef7316747
renarsaar <renar.saaremets@gmail.com>
2021-04-07 21:25:41 +0300

    no state direct reassigning

--- client/src/reducers/cartReducer.js
+++ client/src/reducers/cartReducer.js
@@ -6,66 +6,72 @@ import {
 } from '../actions/types';
 
 const INITIAL_STATE = JSON.parse(sessionStorage.getItem('cart')) || [];
+let newState = [];
 
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case ADD_CART:
+      newState = [...state];
       // If the product already exists in cart
       if (state.some((product) => product.key === action.product.key)) {
         // Find the index of the product
         const index = state.findIndex((product) => product.key === action.product.key);
 
         // Add quantity and price
-        state[index].quantity += action.product.quantity;
-        state[index].totalPrice += action.product.totalPrice;
+        newState[index].quantity += action.product.quantity;
+        newState[index].totalPrice += action.product.totalPrice;
 
         // If more products in cart than in stock
         if (state[index].quantity >= action.product.stock) {
-          state[index].quantity = action.product.stock;
-          state[index].totalPrice = action.product.productPrice * action.product.stock;
+          newState[index].quantity = action.product.stock;
+          newState[index].totalPrice = action.product.productPrice * action.product.stock;
         }
       } else {
-        state = [...state, action.product];
+        newState = [...state, action.product];
       }
 
-      sessionStorage.setItem('cart', JSON.stringify(state));
+      sessionStorage.setItem('cart', JSON.stringify(newState));
 
-      return JSON.parse(sessionStorage.getItem('cart'));
+      return newState;
 
     case ADD_CART_ONE:
+      newState = [...state];
+
       // Add quantity & price
-      state[action.index].quantity += 1;
-      state[action.index].totalPrice += state[action.index].productPrice;
+      newState[action.index].quantity += 1;
+      newState[action.index].totalPrice += state[action.index].productPrice;
 
       if (state[action.index].quantity >= state[action.index].stock) {
-        state[action.index].quantity = state[action.index].stock;
-        state[action.index].totalPrice = state[action.index].productPrice * state[action.index].stock;
+        newState[action.index].quantity = state[action.index].stock;
+        newState[action.index].totalPrice = state[action.index].productPrice * state[action.index].stock;
       }
 
-      sessionStorage.setItem('cart', JSON.stringify(state));
+      sessionStorage.setItem('cart', JSON.stringify(newState));
 
-      return JSON.parse(sessionStorage.getItem('cart'));
+      return newState;
 
     case REMOVE_CART:
+      newState = [...state];
+
       // Subtract quantity & price
-      state[action.index].quantity -= 1;
-      state[action.index].totalPrice -= state[action.index].productPrice;
+      newState[action.index].quantity -= 1;
+      newState[action.index].totalPrice -= state[action.index].productPrice;
 
       if (state[action.index].quantity === 0) {
-        state = [...state.filter((product, index) => index !== action.index)];
+        newState = [...state.filter((product, index) => index !== action.index)];
       }
 
-      sessionStorage.setItem('cart', JSON.stringify(state));
+      sessionStorage.setItem('cart', JSON.stringify(newState));
 
-      return JSON.parse(sessionStorage.getItem('cart'));
+      return newState;
 
     case CLEAR_CART:
-      state = [];
+      newState = [];
       sessionStorage.removeItem('cart');
 
-      return state;
+      return newState;
 
     default:
-      return state;
+      return newState;
   }
 };
