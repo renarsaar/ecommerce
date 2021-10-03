23622d60a980ec070a2dab0cb5bc4e850ecdd311
renarsaar <renar.saaremets@gmail.com>
2021-10-04 00:59:09 +0300

    Linting

--- client/src/Components/Fabric.js
+++ client/src/Components/Fabric.js
@@ -8,13 +8,22 @@ export default function Fabric() {
           <h1 className="mb-1 mt-2">Fabrics 101: How we Choose the Right Fabric for Our Project</h1>
 
           <p className="mb-3">
-            VRA also provides customers the ability to choose our custom tailored clothing. We think it’s so important to have clothes that fit well and make a statement about who you are.
+            VRA also provides customers the ability to choose our custom tailored clothing.
+            We think it’s so important to have clothes that fit well
+            and make a statement about who you are.
 
-            Choosing the right fabric when creating a new project is one of the most important steps we take. If we make the wrong choice, this can greatly affect our finished product.
+            Choosing the right fabric when creating a new project
+            is one of the most important steps we take.
+            If we make the wrong choice, this can greatly affect our finished product.
 
-            In making the correct choice, we are moved either by the fabric’s texture or design. Finding a happy medium between the two is sewing heaven, indeed!
+            In making the correct choice,
+            we are moved either by the fabric’s texture or design.
+            Finding a happy medium between the two is sewing heaven, indeed!
 
-            For those who do not have a lot of time to research the different fabric types, this infographic from Blinds.com is a quick cheat sheet on what you need to know. Learn all about fabrics to help you pick the right material for sewing garments or textile projects.
+            For those who do not have a lot of time to research the different fabric types,
+            this infographic from Blinds.com is a quick cheat sheet on what you need to know.
+            Learn all about fabrics to help you
+            pick the right material for sewing garments or textile projects.
           </p>
         </div>
 
@@ -28,7 +37,9 @@ export default function Fabric() {
       <div className="fabric-article">
         <h3 className="mb-1">Why do we put so much effort in custom clothing?</h3>
         <p>
-          Nothing beats the look of custom crafted clothing. Patterns we make ourselves will show off unique style and personality and let You stand out from the crowd.
+          Nothing beats the look of custom crafted clothing.
+          Patterns we make ourselves will show off unique style and personality
+          and let You stand out from the crowd.
         </p>
       </div>
     </div>
