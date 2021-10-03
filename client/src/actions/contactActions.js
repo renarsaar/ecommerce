f6469727134272d4a3615f65ac2e55749bb70a05
renarsaar <renar.saaremets@gmail.com>
2021-10-04 01:39:47 +0300

    Change duplicate variable 'name'

--- client/src/actions/contactActions.js
+++ client/src/actions/contactActions.js
@@ -3,7 +3,7 @@ import {
   CONTACT_REQUEST,
   CONTACT_REQUEST_ERROR,
   CLEAR_CONTACT_REDUCER,
-} from '../actions/types';
+} from './types';
 import api from '../api';
 
 // Submit a contact form
@@ -11,14 +11,14 @@ export const contactRequest = (values) => async (dispatch) => {
   dispatch({ type: CONTACT_REQUEST_LOADING });
 
   api.post('/contact', {
-    name: values.name,
-    email: values.email,
-    message: values.message,
+    name: values.contactName,
+    email: values.contactEmail,
+    message: values.contactMessage,
   })
     .then((response) => dispatch({ type: CONTACT_REQUEST, payload: response.data }))
     .catch((error) => dispatch({ type: CONTACT_REQUEST_ERROR, payload: error.response.data }));
-}
+};
 
 export const clearContactReducer = () => (dispatch) => {
   dispatch({ type: CLEAR_CONTACT_REDUCER });
-}
+};
